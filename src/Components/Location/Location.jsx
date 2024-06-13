207de7e6cf07969b291e830f305a6553f2bb5eff
MohammadKerdasi <mohammadkerdasi003@gmail.com>
2024-06-13 18:45:53 +0200

    root edited

--- src/Components/Location/Location.jsx
+++ src/Components/Location/Location.jsx
@@ -7,7 +7,7 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 const Location = () => {
   return (
     <>
-    <div className='d-flex Mk-padding-iframe w-100 justify-content-between'>
+    <div className='d-flex Mk-padding-iframe w-100 justify-content-between gap-4'>
         <div className='Mk-ContactTitle'>
             <h1>Need a direct line?</h1>
             <p>Cras massa et odio donec faucibus in. Vitae pretium massa dolor ullamcorper lectus elit quam.</p>
@@ -35,12 +35,16 @@ const Location = () => {
         </div>
         <div className='Mk-Iframe'>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3355378.891023664!2d41.64323629402979!3d34.7873906581411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1518e6dc413cc6a7%3A0x877546f4882af620!2z2LPZiNix2YrYpw!5e0!3m2!1sar!2s!4v1718242439712!5m2!1sar!2s" 
-        width="603.3" height="319.5"
+        // width="603.3" height="319.5"
         allowfullscreen
         loading="lazy" referrerpolicy="no-referrer-when-downgrade"
         title="responsive google map" ></iframe>
         
         </div>
+    </div>
+    <div className='Mk-Contact-Us'>
+        <h1>Contact Us</h1>
+        <p>Your email address will not be published. Required fields are marked *</p>
 
     </div>
     
