7a0b1207ccc8ba58e8a242335c930f081ab3dfff
nala-77 <nahlaabdullhamid@gmail.com>
2024-06-13 16:12:11 +0100

    nahla is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fas, far, fal } from '@awesome.me/kit-KIT_CODE/icons'
 function App() {
 
   return (
     <>
       <h1>Vite + React</h1>
+      <p><FontAwesomeIcon icon={fas.faHouse} />
+    <FontAwesomeIcon icon={far.faMouse} />
+    <FontAwesomeIcon icon={fal.faCheese} /></p>
     </>
   )
 }
