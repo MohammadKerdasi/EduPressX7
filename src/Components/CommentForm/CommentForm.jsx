d71374ab2302dc53ecb7ac8b96740c4f1b239afd
MohammadKerdasi <mohammadkerdasi003@gmail.com>
2024-06-15 01:52:49 +0200

    Contact + FAQs

--- src/Components/CommentForm/CommentForm.jsx
+++ src/Components/CommentForm/CommentForm.jsx
@@ -1,13 +1,15 @@
-import React from "react";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
+import ContactTitle from "../ContactTitle/ContactTitle";
 import './CommentForm.css'
 
-const CommentForm = () => {
-  return (
-    <>
+
+    const CommentForm  = () => {
+        return (
+            <>
+            <ContactTitle  Title='Contact Us' subTitle='Your email address will not be published. Required fields are marked *'/>
         <Form className="Mk-Form">
             <Row className="Mk-Coulmn">
             <Col className="Mk-form-padding">
@@ -31,7 +33,7 @@ const CommentForm = () => {
                 as="textarea"
                 placeholder="Comment"
                 style={{ height: "110px" }}
-            />
+                />
             </FloatingLabel>
             <div className="Mk-checkbox">
             <Form.Check aria-label="option 1" />
@@ -45,4 +47,6 @@ const CommentForm = () => {
     );
 };
 
+
 export default CommentForm;
+
--- src/Pages/FAQs.jsx
+++ src/Pages/FAQs.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
 import BreadCrumbs from '../Components/BreadCrumbs/BreadCrumbs'
-import FAQs from '../Components/FAQs/FAQs'
 import "./../index.css"
+import FAQsPage from '../Components/FAQsPage/FAQsPage'
 
-export default function fAQs() {
+
+
+export default function FAQs() {
   return (
     <div>
-    <BreadCrumbs />
-    <div className='Project-Container'>
-       <FAQs/>
+    <BreadCrumbs/>
+    <div className='Project-Container-FAQs'>
+       <FAQsPage/>
        </div>
     </div>
   )
