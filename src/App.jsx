b9ea09a31848ba1ade070f8449846f6812db112c
MohammadKerdasi <mohammadkerdasi003@gmail.com>
2024-06-18 02:06:53 +0200

    Course single not finished yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import NavBarCom from './Components/NavBar/NavBarCom';
---
+++ src/Components/CourseSingleDetail/CourseSingleDetail.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './CourseSingleDetail.css'
+
+const CourseSingleDetail = () => {
+
+  }
+
+  return (
+    <>
+      <div className='Mk-Tabs-Section'>
+        <div className='Mk-Blocknpm-Tabs'></div>
+        <div></div>
+
+       </div>
+    </>
+  )
+}
+
+export default CourseSingleDetail
