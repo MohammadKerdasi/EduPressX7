f916b86f400db9feb3d5632a3b8456eb27cc869b
nourzidan <nourzidan69@gmail.com>
2024-06-20 21:04:12 +0300

    after chack

---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,22 @@
+
+import "./Error.css"
+
+
+
+export default function Error(){
+    return(
+        <>
+        <section className="nz-error-section">
+
+        <div className="nz-h2-div"><h2>Error</h2></div>
+        <div className="nz-img-div"><img src="./public/assetsProject/imges/error.png" /></div>
+
+        </section>
+        
+        
+        
+        
+        
+        </>
+    )
+}
