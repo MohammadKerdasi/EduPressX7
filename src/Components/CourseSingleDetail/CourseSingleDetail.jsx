3ed8e28a64e6a20d484bbd316aea01f1affbabcd
MohammadKerdasi <mohammadkerdasi003@gmail.com>
2024-07-08 03:02:09 +0200

    some changes

--- src/Components/CourseSingleDetail/CourseSingleDetail.jsx
+++ src/Components/CourseSingleDetail/CourseSingleDetail.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./CourseSingleDetail.css";
 //  import Col from "react-bootstrap/Col";
 //  import Form from "react-bootstrap/Form";
@@ -62,8 +62,44 @@ const Overview = () => {
   );
 };
  import './../CourseSingleAccordions/CourseSingleAccordions.css'
+ import arrow_icon from './../../../public/assetsProject/imges/navBar-Vector.png';
 const Curriculum = () => {
+  const Accordion = ({ title, subTitle, subTitle2, children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const contentRef = useRef(null);
+    
+  
+    useEffect(() => {
+      if (isOpen) {
+        contentRef.current.style.maxHeight = contentRef.current.scrollHeight + 'px';
+      } else {
+        contentRef.current.style.maxHeight = '0px';
+      }
+    }, [isOpen]);
+  
+    return (
+      <div className="Mk-accordion">
+        <div className="Mk-accordion-titleAndSub" onClick={() => setIsOpen(!isOpen)}>
+          <div className={`Mk-AccordionTitle ${isOpen ? 'open' : ''}`} >
+            <img
+              className={`Mk-AccordionArrowIcon ${isOpen ? 'open' : ''}`}
+              src={arrow_icon}
+              alt="arrow icon"
+            />
+            {title}
+          </div>
+          <div className='Mk-AccordionsubTitle'>
+            <span>{subTitle}</span>
+            <span>{subTitle2}</span>
+          </div>
+        </div>
+        <div ref={contentRef} className={`Mk-accordion-content ${isOpen ? 'open' : ''}`}>
+          {children}
+        </div>
+      </div>
+    );
 
+  };
   return (
     <div>
       <p className="Mk-OverViewP">
